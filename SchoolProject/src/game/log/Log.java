e2886f9e0d1ee971ddd41137d7fb4652233ba056
vrl <vrl@rb-lap-vrl.recommind.com>
2014-02-06 14:22:22 +0100

    #1: Added logging.

---
+++ SchoolProject/src/game/log/Log.java
@@ -0,0 +1,17 @@
+package game.log;
+
+public class Log
+{
+	private Log()
+	{}
+	
+	public static void log(String aMessage)
+	{
+		System.out.println("[I] - " + aMessage);
+	}
+	
+	public static void error(String aMessage)
+	{
+		System.out.println("[E] - " + aMessage);
+	}
+}
