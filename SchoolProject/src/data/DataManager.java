bf09189ffde28c258e9cb194ec6aad8fcd3406b0
vrl <vrl@rb-lap-vrl.recommind.com>
2014-02-07 13:34:55 +0100

    #3: Added DataManager.

--- SchoolProject/src/data/DataManager.java
+++ SchoolProject/src/data/DataManager.java
@@ -1,6 +1,35 @@
 package data;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import javax.imageio.ImageIO;
+import org.newdawn.slick.util.Log;
+
 public class DataManager
-{	
+{
+	private static final HashMap<String, BufferedImage>	IMAGES	= new HashMap<>();
 	
+	public static BufferedImage get(String aName)
+	{
+		BufferedImage image = IMAGES.get(aName);
+		if (image == null) image = load(aName);
+		return image;
+	}
+	
+	private static BufferedImage load(String aName)
+	{
+		try
+		{
+			return ImageIO.read(new FileInputStream(new File("data/" + aName
+					+ ".png")));
+		}
+		catch (IOException e)
+		{
+			Log.error("Could not read Image " + aName);
+		}
+		return null;
+	}
 }
