86d32d674d3fb182767cc6b0870369454c7e1780
Vincent <Vincent@Vincent-PC.localdomain>
2014-02-16 16:49:31 +0100

    #11: Made background flow slower.

--- SchoolProject/src/game/world/World.java
+++ SchoolProject/src/game/world/World.java
@@ -236,8 +236,8 @@ public class World
 	public void render(Graphics g)
 	{
 		// Render background
-		g.drawImage(DataManager.getImage("background0"), -(mScreen.getX() % mScreen.getWidth()), 0);
-		g.drawImage(DataManager.getImage("background0"), mScreen.getWidth() - (mScreen.getX() % mScreen.getWidth()), 0);
+		g.drawImage(DataManager.getImage("background0"), -(mScreen.getX() / 5 % mScreen.getWidth()), 0);
+		g.drawImage(DataManager.getImage("background0"), mScreen.getWidth() - (mScreen.getX() / 5 % mScreen.getWidth()), 0);
 		
 		// Render Blocks
 		for (int x = Math.max(mScreen.getX() / Block.SIZE, 0); x <= (mScreen.getX() + mScreen.getWidth()) / Block.SIZE && x < mWidth; x++ )
