eb6fc21acaa615b533c6f139694bc27c83ca6d3b
Valentin <Valentin@192.168.2.101>
2014-03-01 01:14:46 +0100

    #36 Finally added the stonetexture and a lvl for stone.

--- SchoolProject/src/game/level/block/Texture.java
+++ SchoolProject/src/game/level/block/Texture.java
@@ -15,6 +15,7 @@ public class Texture
 	public static final Texture 				CLOUD 	= new Texture(5, "Cloud");
 	public static final Texture					JUNGLE	= new Texture(6, "Jungle");
 	public static final Texture					HORROR	= new Texture(7, "Horror");
+	public static final Texture					STONE 	= new Texture(8, "Stone");
 	
 	private final byte							mId;
 	
