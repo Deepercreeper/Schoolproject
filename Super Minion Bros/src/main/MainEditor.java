b4b2a0f4c51c5cc01d42d60c83d75c3c983c264c
Vincent <Vincent@Vincent-PC.localdomain>
2014-04-05 14:37:14 +0200

    Editor Main.

---
+++ Super Minion Bros/src/main/MainEditor.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import editor.Editor;
+
+public class MainEditor
+{
+	public static void main(final String[] args)
+	{
+		try
+		{
+			new Editor();
+		}
+		catch (final Exception e)
+		{
+			final String userHomeFolder = System.getProperty("user.home");
+			final File log = new File(userHomeFolder + "/Desktop", "log.txt");
+			try
+			{
+				log.createNewFile();
+				final BufferedWriter writer = new BufferedWriter(new FileWriter(log));
+				final String message = e.getMessage();
+				if (message.startsWith("Resource not found")) writer.write("Data Verzeichnis fehlt!");
+				else writer.write(message);
+				writer.close();
+			}
+			catch (final IOException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+	}
+}
