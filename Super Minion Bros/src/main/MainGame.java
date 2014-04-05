ed728119bbae181e1363fbe49b56f098b524e6ae
Vincent <Vincent@Vincent-PC.localdomain>
2014-04-05 14:36:59 +0200

    Differenziated between game and editor.

--- Super Minion Bros/src/main/MainGame.java
+++ Super Minion Bros/src/main/MainGame.java
@@ -4,29 +4,15 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import org.newdawn.slick.SlickException;
 import view.View;
-import editor.Editor;
 
-public class Main
+public class MainGame
 {
 	public static void main(final String[] args)
 	{
 		try
 		{
-			if (args.length > 0 && args[0].equals("Editor"))
-			{
-				new Editor();
-				return;
-			}
-			try
-			{
-				new View();
-			}
-			catch (final SlickException e)
-			{
-				e.printStackTrace();
-			}
+			new View();
 		}
 		catch (final Exception e)
 		{
